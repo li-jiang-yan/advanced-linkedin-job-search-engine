675a46ffc45cc647ebe1c0237e8f25cb915e783b
li-jiang-yan <professional.jiangyan_li@outlook.com>
2026-06-21 01:16:20 +0800

    [#71] updated lib.search.search_jobs to fetch jobs from the LinkedIn API

--- lib/search.py
+++ lib/search.py
@@ -1,18 +1,31 @@
 from urllib.parse import urlencode
 
+import requests
+from bs4 import BeautifulSoup
+
 
 def search_jobs(request):
     params = request.args.to_dict(flat=True)
     number = int(params.pop("number", 1))
     query = urlencode(params)
+
     base_url = "https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search"
-    urls = []
+    results = []
 
-    for start in range(number):
-        if query:
-            url = f"{base_url}?{query}&start={start}"
+    headers = {
+        "User-Agent": "Mozilla/5.0",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    }
+
+    start = 0
+    while start < number:
+        url = f"{base_url}?{query}&start={start}"
+        response = requests.get(url, headers=headers, timeout=30)
+        if response.status_code == 200:
+            result = BeautifulSoup(response.text, "html.parser").find_all("li")
+            results += [str(elem) for elem in result]
+            start += len(result)
         else:
-            url = f"{base_url}?start={start}"
-        urls.append(url)
+            start += 1
 
-    return {"urls": urls}, 200
+    return {"results": results}, 200
