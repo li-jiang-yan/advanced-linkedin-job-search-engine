fa77df5a1324b224f92ba5fc9cf7ce23eace089f
li-jiang-yan <professional.jiangyan_li@outlook.com>
2026-06-26 22:08:22 +0800

    [#73] added parsing of results from LinkedIn API

--- lib/search.py
+++ lib/search.py
@@ -29,7 +29,11 @@ def search_jobs(params):
         response.raise_for_status()
 
         soup = BeautifulSoup(response.text, "html.parser")
-        jobs.extend(str(element) for element in soup.find_all("li"))
+        jobs.extend(
+            _parse_job_html(element)
+            for element in soup.find_all("li")
+            if _parse_job_html(element)
+        )
         start += len(soup.find_all("li"))
 
     return jobs
@@ -39,3 +43,22 @@ def _build_search_url(query_string, start):
     if query_string:
         return f"{LINKEDIN_SEARCH_URL}?{query_string}&start={start}"
     return f"{LINKEDIN_SEARCH_URL}?start={start}"
+
+
+def _parse_job_html(soup):
+    try:
+        link_el = soup.select_one(".base-card__full-link").get("href")
+        title_el = soup.select_one(".base-search-card__title").get_text().strip()
+        company_el = soup.select_one(".base-search-card__subtitle").get_text().strip()
+        location_el = soup.select_one(".job-search-card__location").get_text().strip()
+        date_el = soup.select_one(".job-search-card__listdate").get("datetime")
+    except Exception as _:
+        return None
+    else:
+        return {
+            "link": link_el,
+            "title": title_el,
+            "company": company_el,
+            "location": location_el,
+            "date_posted": date_el,
+        }
